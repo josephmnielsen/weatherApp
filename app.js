b807a56c63df37fbbb648e5a345f477c5bbc5cff
Joseph Nielsen <josephmnielsen@gmail.com>
2021-02-25 13:23:36 -0800

    styling update

--- app.js
+++ app.js
@@ -30,41 +30,41 @@ axios.get(`https://api.openweathermap.org/data/2.5/weather?units=imperial&q=irvi
   .then(res => {
 
     document.getElementById('dayOne').innerHTML = `
-    <p>${tomorrow}</p>
+    
     <img class="card-img-top" src="http://openweathermap.org/img/wn/${res.data.list[0].weather[0].icon}@2x.png">
-    <br>
+    <p>${tomorrow}</p>
     <p class="card-text">Temp: ${Math.round(res.data.list[0].main.temp)}°F</p>
     <p class="card-text">Humidity: ${res.data.list[0].main.humidity}%</p>
     <p class="card-text">Winds: ${Math.round(res.data.list[0].wind.speed)}mph</p>
     `
     document.getElementById('dayTwo').innerHTML = `
-    <p>${dayTwo}</p>
+    
     <img class="card-img-top" src="http://openweathermap.org/img/wn/${res.data.list[1].weather[0].icon}@2x.png">
-    <br>
+    <p>${dayTwo}</p>
     <p class="card-text">Temp: ${Math.round(res.data.list[1].main.temp)}°F</p>
     <p class="card-text">Humidity: ${res.data.list[1].main.humidity}%</p>
     <p class="card-text">Winds: ${Math.round(res.data.list[1].wind.speed)}mph</p>
     `
     document.getElementById('dayThree').innerHTML = `
-    <p>${dayThree}</p>
+    
     <img class="card-img-top" src="http://openweathermap.org/img/wn/${res.data.list[2].weather[0].icon}@2x.png">
-    <br>
+    <p>${dayThree}</p>
     <p class="card-text">Temp: ${Math.round(res.data.list[2].main.temp)}°F</p>
     <p class="card-text">Humidity: ${res.data.list[2].main.humidity}%</p>
     <p class="card-text">Winds: ${Math.round(res.data.list[2].wind.speed)}mph</p>
     `
     document.getElementById('dayFour').innerHTML = `
-    <p>${dayFour}</p>
+    
     <img class="card-img-top" src="http://openweathermap.org/img/wn/${res.data.list[3].weather[0].icon}@2x.png">
-    <br>
+    <p>${dayFour}</p>
     <p class="card-text">Temp: ${Math.round(res.data.list[3].main.temp)}°F</p>
     <p class="card-text">Humidity: ${res.data.list[3].main.humidity}%</p>
     <p class="card-text">Winds: ${Math.round(res.data.list[3].wind.speed)}mph</p>
     `
     document.getElementById('dayFive').innerHTML = `
-    <p>${dayFive}</p>
+    
     <img  src="http://openweathermap.org/img/wn/${res.data.list[4].weather[0].icon}@2x.png">
-    <br>
+    <p>${dayFive}</p>
     <p class="card-text">Temp: ${Math.round(res.data.list[4].main.temp)}°F</p>
     <p class="card-text">Humidity: ${res.data.list[4].main.humidity}%</p>
     <p class="card-text">Winds: ${Math.round(res.data.list[4].wind.speed)}mph</p>
@@ -81,7 +81,7 @@ axios.get(`https://api.openweathermap.org/data/2.5/weather?units=imperial&q=irvi
   document.getElementById('list-group').append(listElem)
 
   let city = document.getElementById('search').value
-  
+
    document.getElementById('search').value = ""
  
   axios.get(`https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=2bd9578496cb426fc00d78ee8b1ad6d0`)
